63c6b09887a36d7405e48633bcebb69d46aa71d6
undecipherable <82640195+undecipherable@users.noreply.github.com>
2021-07-09 18:12:28 +0100

    adds task 2 project file, cleaning script, analysis

--- task_1/data_cleaning_scripts/decathlon_cleaning_script.R
+++ task_1/data_cleaning_scripts/decathlon_cleaning_script.R
@@ -14,7 +14,7 @@ decathlon_cleaned <- decathlon_data %>%
   mutate(id = cur_group_id()) %>%
 # Clean names, rearrange columns and drop columns not needed for analysis
   clean_names() %>%
-  select(id, name, competition, points, long_jump, x100m, shot_put, x400m)
+  select(id, name, competition, points, rank, long_jump, x100m, shot_put, x400m)
 
 # Write data to .csv
 write_csv(decathlon_cleaned, here("clean_data/decathlon_cleaned.csv"))
---
+++ task_2/data_cleaning_scripts/cake_ingredients_cleaning_script.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(janitor)
+library(here)
+
+# Read in cake ingredient data and cake code data
+cake_ingredients <- read_csv(here("raw_data/cake-ingredients-1961.csv"))
+cake_ingredient_names <- read_csv(here("raw_data/cake_ingredient_code.csv"))
+
+# Pivot into long format and drop NA values in quantity then clean names
+cake_ingredients_long <- cake_id %>%
+  pivot_longer(-Cake, names_to = "code", values_to = "quantity") %>%
+  drop_na(quantity) %>%
+  clean_names()
+
+# Join tables by cake codes
+cake_joined <- cake_ingredients_long %>%
+  left_join(cake_ingredient_names, by = c("code" = "code")) 
+
+# There are some NA values by Sour cream where it should say "cup", replace the
+# "Sour cream cup" strings with "Sour cream" and replace NA with "cup.
+cake_tidy <- cake_joined %>%
+  mutate(ingredient = str_replace_all(ingredient, "Sour cream cup", "Sour cream"),
+         measure = replace_na(measure, "cup")) %>%
+  # remove code from table
+  select(-code)
+
+# write cleaned data to .csv
+write_csv(cake_tidy, here("clean_data/cake_ingredients_cleaned.csv"))
+          
