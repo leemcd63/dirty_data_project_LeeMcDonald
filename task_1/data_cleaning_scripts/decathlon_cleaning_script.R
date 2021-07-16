5503f6c2982a2eff5132e2df85d42d0d4732b79f
undecipherable <82640195+undecipherable@users.noreply.github.com>
2021-07-16 09:48:14 +0100

    task1 and task 6 complete

--- task_1/data_cleaning_scripts/decathlon_cleaning_script.R
+++ task_1/data_cleaning_scripts/decathlon_cleaning_script.R
@@ -7,16 +7,19 @@ decathlon_data <- read_rds(here("raw_data/decathlon.rds"))
 decathlon_cleaned <- decathlon_data %>%
   # Change row names to column called "name"
   rownames_to_column(var = "name") %>%
-  # Create id for each observation, convert athlete names to same case
-  mutate(id = row_number(),
-         name = str_to_sentence(name)) %>%
-  # Create unique id for each name
+  # Create unique id for each athlete
+  mutate(name = str_to_title(name)) %>%
   group_by(name) %>%
   mutate(athlete_id = cur_group_id()) %>%
+  ungroup() %>%
   # rearrange columns and drop columns not needed for analysis
-  select(id, athlete_id, name, Competition, Points, Rank, Long.jump, "100m", Shot.put, "400m") %>%
+  select(athlete_id, name, Competition, Points, Rank, Long.jump, "100m", Shot.put, "400m") %>%
   # Convert into long format
   pivot_longer(Long.jump:"400m", names_to = "event", values_to = "score") %>%
+  # Create id for each observation
+  mutate(id = row_number()) %>%
+  # Move id column to front
+  select(id, everything()) %>%
   # clean column names
   clean_names() %>%
   # replace "." in event names with a space
